d4727d63bb07424e1cd9606a511c47715357b568
Jeff Roche <jkeauctions@gmail.com>
2022-08-28 16:30:13 -0400

    Added objects and returning a list of objects

--- api/models/player.py
+++ api/models/player.py
@@ -1,5 +1,13 @@
 class Player:
-    attrs = ['id', 'name', 'age', 'height', 'weight', 'shot', 'birth_place', 'birthdate', 'number']
+    __table__ = 'players'
+    columns = ['id', 'name', 'age', 'height', 'weight', 'shot', 'birth_place', 'birthdate', 'number', 'created_at']
 
-    def __init__(self, player):
-        self.__dict__ = dict(zip(attrs, player))
+    def __init__(self, kwargs):
+        # make sure all of the keys match column names
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in columns: {self.columns}')
+
+        # set the Player attributes
+        for k, v in kwargs.items():
+            setattr(self, k, v)
